2182ba457cccf8fc531d3abafd96db3eed862ba1
WAKHSAS <waecomeca@gmail.com>
2023-01-28 14:21:33 +0100

    Update File ClientRepository

---
+++ src/main/java/com/ingse/appligestdesclients/AppliGestDesClientsApplication.java
@@ -0,0 +1,13 @@
+package com.ingse.appligestdesclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliGestDesClientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppliGestDesClientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingse/appligestdesclients/AppliGestDesClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingse.appligestdesclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliGestDesClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
